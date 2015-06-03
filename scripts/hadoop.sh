61619ea1b8356b5ebf6687408bc4c62ed20e2bb8
jnjcc <jnjcc@live.com>
2015-06-03 20:20:12 +1200

    hadoop python mapper the template

---
+++ scripts/hadoop.sh
@@ -0,0 +1,42 @@
+## Copyright (c) 2012, 2014 Yste.org
+## All Rights Reserved.
+
+HADOOP_BIN="/path/to/hadoop"
+HADOOP_PYTHON_PATH="hdfs://path/to/python.tar.gz"
+
+HADOOP_MAP_CAPACITY=500
+HADOOP_MAP_TASKS=500
+HADOOP_MAP_FAIL_PERCNT=10
+HADOOP_REDUCE_CAPACITY=500
+HADOOP_REDUCE_TASKS=500
+HADOOP_REDUCE_FAIL_PERCNT=10
+
+hadoop_run() {
+    if [ $# -lt 4 ]; then
+        echo "Usage: hadoop_run JOBNAME INPUTDIR OUTPUTDIR MAPDIR"
+        return
+    fi
+
+    JOBNAME="$1"; INDIR="$2"; OUDIR="$3"; MAPDIR="$4"
+    MAPPRG=`basename ${MAPDIR}`
+
+    $HADOOP_BIN dfs -rmr $OUDIR
+    $HADOOP_BIN streaming \
+        -D mapred.job.priority='NORMAL' \
+        -D mapred.job.name=${JOBNAME} \
+        -D mapred.job.map.capacity=${HADOOP_MAP_CAPACITY} \
+        -D mapred.map.over.capacity.allowed=false \
+        -D mapred.job.map.tasks=${HADOOP_MAP_TASKS} \
+        -D mapred.max.map.failures.percent=${HADOOP_MAP_FAIL_PERCNT} \
+        -D mapred.job.reduce.capacity=${HADOOP_REDUCE_CAPACITY} \
+        -D mapred.reduce.over.capacity.allowed=false \
+        -D mapred.reduce.tasks=${HADOOP_REDUCE_TASKS} \
+        -D mapred.max.reduce.failures.percent=${HADOOP_REDUCE_FAIL_PERCNT} \
+        -D stream.map.output.field.separator='\t' \
+        -D stream.num.map.output.key.fields=1 \
+        -mapper "./python/bin/python ${MAPPRG}" \
+        -input "${INDIR}" \
+        -output "${OUDIR}" \
+        -file "${MAPDIR}" \
+        -cacheArchive "${HADOOP_PYTHON_PATH}#python"
+}
