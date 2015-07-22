ef3252ac6961d21ff0d21841e10277e32bada63d
jnjcc <jnjcc@live.com>
2015-07-22 23:32:46 +1200

    query raw trie for extra word info, the test app

--- app/tests/dtraw_test.cc
+++ app/tests/dtraw_test.cc
@@ -7,18 +7,26 @@
 #include "utils/strings.cc"
 #include <string.h>
 
+static void fill_extra(const char *word, char *extra) {
+  strcpy(extra, word);
+}
+static int comp_extra(const char *word, const char *extra) {
+  return strcmp(word, extra);
+}
+
 static int add_wds_file(datrie *dtraw, const char *fpath) {
   FILE *fin = fopen(fpath, "r");
   if (!fin) {
     return 0;
   }
-  const void *pnull = NULL;
-  int nwds = 0;
   const int kMaxLine = 256;
+  char extra[kMaxLine];
+  int nwds = 0;
   char line[kMaxLine];
   while (fgets(line, kMaxLine, fin)) {
     yaseek::rtrims(line);
-    if (dtadd(dtraw, line, &pnull, sizeof(pnull))) {
+    fill_extra(line, extra);
+    if (dtadd(dtraw, line, extra, strlen(extra) + 1)) {
       ++nwds;
     }
   }
@@ -33,19 +41,27 @@ static void search_wds_file(const datrie *dtraw, const char *fpath) {
   }
 
   int nlost = 0;
+  int ndiff = 0;
   const int kMaxLine = 256;
   char line[kMaxLine];
+  char extra[kMaxLine];
   while (fgets(line, kMaxLine, fin)) {
     yaseek::rtrims(line);
-    if (!find_raw_trie(dtraw, line)) {
+    if (!get_raw_trie(dtraw, line, extra, kMaxLine)) {
       ++nlost;
       fprintf(stderr, "{%s} not in dict...\n", line);
+    } else {
+      if (comp_extra(line, extra) != 0) {
+        ++ndiff;
+        fprintf(stderr, "{%s}'s extra info{%s} wrong\n", line, extra);
+      }
     }
   }
-  if (nlost > 0) {
-    fprintf(stderr, "Oops: %d words lost from raw trie\n", nlost);
+  if (nlost > 0 || ndiff > 0) {
+    fprintf(stderr, "Oops: %d words lost, %d words diff in raw trie\n", nlost,
+        ndiff);
   } else {
-    fprintf(stderr, "Hooray! No words lost from raw trie\n");
+    fprintf(stderr, "Hooray! No words lost, no words diff in raw trie\n");
   }
 }
 
@@ -59,7 +75,7 @@ int main(int argc, char *argv[]) {
   if (argc > 2) {
     fsearch = argv[2];
   }
-  datrie *dtraw = dtalloc(ENC_UTF8, "en_US.UTF8", 10*1024, 0);
+  datrie *dtraw = dtalloc(ENC_UTF8, "en_US.UTF8", 10*1024, 1);
   if (dtraw == NULL) {
     return 0;
   }
