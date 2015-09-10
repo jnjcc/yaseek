f01ca7b9c3753c8001a40a008052da70d32c6eb8
jnjcc <jnjcc@live.com>
2015-09-10 18:27:28 +1200

    hadoop streaming job with python mapper and null reducer
    set reducer to NONE to avoid the extra sort phase

--- scripts/hadoop.sh
+++ scripts/hadoop.sh
@@ -12,10 +12,10 @@ HADOOP_REDUCE_CAPACITY=500
 HADOOP_REDUCE_TASKS=500
 HADOOP_REDUCE_FAIL_PERCNT=10
 
-## hadoop job with python mapper, the template
-hadoop_run() {
+## hadoop streaming job with python mapper and cat reducer, the template
+hadoop_py_cat() {
     if [ $# -lt 4 ]; then
-        echo "Usage: hadoop_run JOBNAME INPUTDIR OUTPUTDIR MAPDIR"
+        echo "Usage: hadoop_py_cat JOBNAME INPUTDIR OUTPUTDIR MAPDIR"
         return
     fi
 
@@ -38,6 +38,39 @@ hadoop_run() {
         -D stream.map.output.field.separator='\t' \
         -D stream.num.map.output.key.fields=1 \
         -mapper "./python/bin/python ${MAPPRG}" \
+        -input "${INDIR}" \
+        -output "${OUDIR}" \
+        -file "${MAPDIR}" \
+        -cacheArchive "${HADOOP_PYTHON_PATH}#python"
+}
+
+## hadoop streaming job with python mapper and null reducer
+hadoop_py_null() {
+    if [ $# -lt 4 ]; then
+        echo "Usage: hadoop_py_null JOBNAME INPUTDIR OUTPUTDIR MAPDIR"
+        return
+    fi
+
+    JOBNAME="$1"; INDIR="$2"; OUDIR="$3"; MAPDIR="$4"
+    MAPPRG=`basename ${MAPDIR}`
+
+    $HADOOP_BIN dfs -rmr $OUDIR
+    $HADOOP_BIN streaming \
+        -D mapred.job.priority='NORMAL' \
+        -D mapred.job.name=${JOBNAME} \
+        -D stream.memory.limit=${HADOOP_MEM_LIMIT} \
+        -D mapred.job.map.capacity=${HADOOP_MAP_CAPACITY} \
+        -D mapred.map.over.capacity.allowed=false \
+        -D mapred.job.map.tasks=${HADOOP_MAP_TASKS} \
+        -D mapred.max.map.failures.percent=${HADOOP_MAP_FAIL_PERCNT} \
+        -D mapred.job.reduce.capacity=${HADOOP_REDUCE_CAPACITY} \
+        -D mapred.reduce.over.capacity.allowed=false \
+        -D mapred.reduce.tasks=${HADOOP_REDUCE_TASKS} \
+        -D mapred.max.reduce.failures.percent=${HADOOP_REDUCE_FAIL_PERCNT} \
+        -D stream.map.output.field.separator='\t' \
+        -D stream.num.map.output.key.fields=1 \
+        -mapper "./python/bin/python ${MAPPRG}" \
+        -reducer NONE \
         -input "${INDIR}" \
         -output "${OUDIR}" \
         -file "${MAPDIR}" \
